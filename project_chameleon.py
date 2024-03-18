f88e4a9ce860ed0ba555d096f8cb6417e91e4546
Petr Vychopeň <petr.vychopen@direct.cz>
2024-03-18 19:19:57 +0100

    json1 vehicle definition

--- project_chameleon.py
+++ project_chameleon.py
@@ -1,1 +1,24 @@
 import json
+
+# Definition of JSON 1 
+json_1 ={
+  "vehicle": {
+    "type": 10,
+    "manufacturerModelId": "054700",
+    "manufacturer": "Iveco",
+    "model": "Magirus",
+    "code": 5,
+    "usePurpose": 1,
+    "manufactureYear": 1999,
+    "countPlace": 2,
+    "maxWeight": 26000,
+    "enginePower": 221,
+    "engineDisplacement": 9500,
+    "leadinDate": "1999-01-06",
+    "vin": "WJME2NMT00C057419",
+    "fuelType": 1,
+    "securityLevel": 0,
+    "registrationPlate": "LNH9785",
+    "registrationBookNumber": ""
+  }
+}
